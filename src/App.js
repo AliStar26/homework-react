e99b9ea0e29ec61f10fa1714e13096304a8a6cb2
Alikhan <alikhanilyassov@gmail.com>
2023-10-21 23:51:02 +0600

    Task1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Мое имя: АЛИХАН</h1>
+      <p>Контактный телефон: +7 707 777 7777</p>
+      <p>Электронный адрес: 7777@gmail.com</p>
     </div>
   );
 }
