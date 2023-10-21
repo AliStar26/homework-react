f5797d9977c77444557185f400f83eb059443b9e
Alikhan <alikhanilyassov@gmail.com>
2023-10-22 00:09:04 +0600

    fix

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import "./App.css";
-import bayterek from "./img/bayterek.jpg";
-import shatyr from "./img/shatyr.jpg";
+
 function App() {
   return (
     <div className="App">
-      <h1>Astana</h1>
-      <p>Страна: Kazakhstan</p>
-      <p>Год основания: 1991</p>
-      <h2>Достопримечательности</h2>
-      <div className="attractions">
-        <img className="photo" src={bayterek} alt="Достопримечательность 1" />
-        <img className="photo" src={shatyr} alt="Достопримечательность 2" />
-      </div>
+      <h1>Мое имя: АЛИХАН</h1>
+      <p>Контактный телефон: +7 707 777 7777</p>
+      <p>Электронный адрес: 7777@gmail.com</p>
     </div>
   );
 }
