280e99c71c25b55ebd76f64f24952687ca711257
Alikhan <alikhanilyassov@gmail.com>
2023-10-22 00:49:52 +0600

    Task4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import "./App.css";
+import Jobs from "./components/Jobs";
+import Shakespeare from "./components/Shakespeare";
 
 function App() {
   return (
     <div className="App">
-      <h1>Мое имя: АЛИХАН</h1>
-      <p>Контактный телефон: +7 707 777 7777</p>
-      <p>Электронный адрес: 7777@gmail.com</p>
+      <Shakespeare />
+      <Jobs
+        title="Гамлет"
+        description="Трагедия Уильяма Шекспира о принце Гамлете Датском."
+      />
+      <Jobs
+        title="Ромео и Джульетта"
+        description="Трагедия Уильяма Шекспира о любви Ромео и Джульетты."
+      />
     </div>
   );
 }
