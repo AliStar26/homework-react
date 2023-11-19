0cfd6beea03a795a58882109054c2872f3760385
Alikhan <alikhanilyassov@gmail.com>
2023-11-19 12:27:33 +0600

    #Art

--- src/App.js
+++ src/App.js
@@ -1,56 +1,36 @@
-import React, { useState } from "react";
-import "./App.css";
-import Display from "./components/Display";
-import Button from "./components/Button";
+// Import necessary components and libraries
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Biography from "./components/Biography";
+import Painting from "./components/Painting";
+import Art from "./components/Art";
 
 function App() {
-  const [input, setInput] = useState("");
-  const [result, setResult] = useState("");
-
-  const handleButtonClick = (value) => {
-    setInput((prevInput) => prevInput + value);
-  };
-
-  const handleCalculate = () => {
-    try {
-      setResult(eval(input).toString());
-    } catch (error) {
-      setResult("Ошибка");
-    }
-  };
-
-  const handleClear = () => {
-    setInput("");
-    setResult("");
-  };
-
   return (
-    <div className="calculator">
-      <Display input={input} result={result} />
-      <div className="buttons">
-        <Button value="1" onClick={() => handleButtonClick("1")} />
-        <Button value="2" onClick={() => handleButtonClick("2")} />
-        <Button value="3" onClick={() => handleButtonClick("3")} />
-        <Button value="4" onClick={() => handleButtonClick("4")} />
-        <Button value="5" onClick={() => handleButtonClick("5")} />
-        <Button value="6" onClick={() => handleButtonClick("6")} />
-        <Button value="7" onClick={() => handleButtonClick("7")} />
-        <Button value="8" onClick={() => handleButtonClick("8")} />
-        <Button value="9" onClick={() => handleButtonClick("9")} />
-        <Button value="0" onClick={() => handleButtonClick("0")} />
-        <Button value="+" onClick={() => handleButtonClick("+")} />
-        <Button value="*" onClick={() => handleButtonClick("*")} />
-        <Button value="/" onClick={() => handleButtonClick("/")} />
-        <Button value="-" onClick={() => handleButtonClick("-")} />
-        <Button value="C" onClick={handleClear} className="clearButton" />
-        <Button
-          value="="
-          onClick={handleCalculate}
-          className="calculateButton"
-        />
-      </div>
-    </div>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/biography">Biography</Link>
+          </li>
+          <li>
+            <Link to="/famous">Painting</Link>
+          </li>
+          <li>
+            <Link to="/art">Art</Link>
+          </li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/biography" element={<Biography />} />
+        <Route path="/famous" element={<Painting />} />
+        <Route path="/art" element={<Art />} />
+      </Routes>
+    </Router>
   );
 }
-
+function Home() {
+  return <div>Welcome to the Home Page!</div>;
+}
 export default App;
---
+++ src/components/Painting.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Painting() {
+  return (
+    <div>
+      <h2>Известная картина</h2>
+      {/* Здесь добавьте изображение или описание известной картины */}
+    </div>
+  );
+}
+
+export default Painting;
